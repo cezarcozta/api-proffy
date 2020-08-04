7b291765450d5ee361543bbcb56e6c8455caf126
César Augusto Costa <cezarcozta@gmail.com>
2020-08-04 18:03:23 -0300

    feat: create classes post route

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,14 @@
 import express from 'express';
+//import cors from 'cors';
+
+import routes from './routes';
 
 const app = express();
 
-app.use(express.json());
+//app.use(cors());
 
-app.get('/', (request, response) => {
-  return response.json({message: 'ok'});
-});
+app.use(express.json());
+app.use(routes);
 
 app.listen(3333);
 
