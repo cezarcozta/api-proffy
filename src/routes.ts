fe5cc68a256d88398925219384c5d10e4dd33cc6
César Augusto Costa <cezarcozta@gmail.com>
2020-08-04 19:27:58 -0300

    feat: create connections routes

--- src/routes.ts
+++ src/routes.ts
@@ -1,67 +1,17 @@
 import express from 'express';
-import db from './database/connection';
-import convertHourToMinutes from './utils/convertHourToMinutes';
+
+import ClassesControler from './controllers/ClassesController';
+import ConnectionsControler from './controllers/ConnectionsController';
 
 const routes = express.Router();
 
-interface ScheduleItem {
-  week_day: number;
-  from: string;
-  to: string;
-}
+const classController = new ClassesControler();
+const connectionsController = new ConnectionsControler();
 
-routes.post('/classes', async (request, response) => {
-  const {
-    name,
-    avatar,
-    whatsapp,
-    bio,
-    subject,
-    cost,
-    schedule
-  } = request.body;
+routes.post('/classes', classController.create);
+routes.get('/classes', classController.index);
 
-  const trx = await db.transaction();
-
-  try {
-    const insertedUsersIDs = await trx('users').insert({
-      name,
-      avatar,
-      whatsapp,
-      bio,
-    });
-  
-    const user_id = insertedUsersIDs[0];
-  
-    const insertedClassesIDs = await trx('classes').insert({
-      subject,
-      cost,
-      user_id,
-    });
-  
-    const class_id = insertedClassesIDs[0];
-  
-    const classSchedule =  schedule.map((scheduleItem: ScheduleItem) => {
-      return {
-        class_id,
-        week_day: scheduleItem.week_day,
-        from: convertHourToMinutes(scheduleItem.from),
-        to: convertHourToMinutes(scheduleItem.to)
-      }
-    });
-  
-    await trx('class_schedule').insert(classSchedule);
-  
-    await trx.commit();
-  
-    return response.status(201).send();
-  } catch (error) {
-    await trx.rollback();
-
-    return response.status(400).json({
-      error: 'Unexpected error while creating new class'
-    });
-  }
-});
+routes.post('/connections', connectionsController.create);
+routes.get('/connections', connectionsController.index);
 
 export default routes;
